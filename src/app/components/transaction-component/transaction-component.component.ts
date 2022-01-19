8ca80a9f4c874bc2d064762fa9e1c30ec4c434e9
Oscarmageddon <oscarmageddon@gmail.com>
2022-01-19 16:41:35 -0300

    se agrega funcionalidad de getAll para grilla

--- src/app/components/transaction-component/transaction-component.component.ts
+++ src/app/components/transaction-component/transaction-component.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { TransactionService } from '../../services/transaction.service';
 import { Transaction } from '../../interfaces/transaction.interface';
+import { compileNgModuleDeclarationExpression } from '@angular/compiler/src/render3/r3_module_compiler';
 
 @Component({
   selector: 'app-transaction-component',
@@ -8,6 +9,8 @@ import { Transaction } from '../../interfaces/transaction.interface';
   styleUrls: ['./transaction-component.component.scss']
 })
 export class TransactionComponentComponent implements OnInit {
+
+  transactions: Array<Transaction> = new Array<Transaction>() ;
 
   nomUser: string = '';
   apeUser: string = '';
@@ -27,10 +30,14 @@ export class TransactionComponentComponent implements OnInit {
   constructor(private transactionService: TransactionService) { }
 
   ngOnInit(): void {
+    this.transactionService.getAll()
+      .subscribe(transactions => {
+        this.transactions = transactions;
+        console.log('Transactions', transactions);
+      });
   }
 
   guardar() {
-
     this.trx = {
       nombreUsr: this.nomUser,
       apellidoUsr: this.apeUser,
@@ -40,27 +47,25 @@ export class TransactionComponentComponent implements OnInit {
     }
 
     this.transactionService.saveTransaction(this.trx)
-      .subscribe(
-        (resp) => {
-         console.log('Respuesta ', resp)
-        },
-        (respError) => {
+      .subscribe({
+        next: (resp) => console.log('Respuesta ', resp),
+        error: (respError) => {
           console.log(respError.error);
           this.errorMessage = respError.error.errorMessage;
         },
-      )
+      })
   }
 
   buscarPorDni() {
 
     this.transactionService.findTransactionByDni(this.dniUser)
-      .subscribe(resp => {
+      .subscribe({
+        next: (resp) => {
         console.log('Respuesta ', resp);
         this.nomUser = resp.nombreUsr;
         this.apeUser = resp.apellidoUsr;
         this.dniUser = resp.dniUsr;
         this.payMethod = resp.paymentMethod;
-
         this.trx = {
           nombreUsr: resp.nombreUsr,
           apellidoUsr: resp.apellidoUsr,
@@ -70,11 +75,10 @@ export class TransactionComponentComponent implements OnInit {
           id: resp.id
         }
       },
-      (respError) => {
+      error: (respError) => {
         console.log(respError.error);
         this.errorMessage = respError.error.errorMessage;
-      }
-      )
+      }})
   }
 
   actualizarEstadoDni() {
@@ -86,6 +90,4 @@ export class TransactionComponentComponent implements OnInit {
         })
     }
   }
-
-
 }
